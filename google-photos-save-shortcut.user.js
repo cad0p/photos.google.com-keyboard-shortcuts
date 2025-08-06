66a32f0f8c6700bddd8a5e9ebfe14898f450b31d
Pier Carlo Cadoppi <pc.cadoppi@gmail.com>
2025-08-06 23:25:38 +0900

    created script

---
+++ google-photos-save-shortcut.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Google Photos Save Shortcut
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Press 's' to save photo from shared album
+// @author       You
+// @match        https://photos.google.com/share/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    document.addEventListener('keydown', function(e) {
+        if (e.key === 's' && !e.ctrlKey && !e.metaKey && !e.altKey) {
+            const saveButton = document.querySelector('button[jsname="LgbsSe"] span[jsname="V67aGc"]');
+            if (saveButton && saveButton.textContent === 'Save') {
+                e.preventDefault();
+                saveButton.closest('button').click();
+            }
+        }
+    });
+})();
